7a62ca0c8efbaf74b0805a8e95bd60fb84323610
atefelgohary2030 <atef.elgohary@gmail.com>
2026-01-29 11:52:14 +0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
-// Future Dimensions PWA Service Worker (v4.0.2)
-const VERSION = "fd-pwa-v4.0.2"
+// Future Dimensions PWA Service Worker (v4)
+const VERSION = "fd-pwa-v4.0.1"
 const CACHE_NAME = VERSION
 
 const scopeURL = () => {
